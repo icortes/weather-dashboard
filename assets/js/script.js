801ddc736497f73e3e75c8f45db6e5d13a32f604
isa <5445946+icortes@users.noreply.github.com>
2021-10-04 20:41:44 -0700

    updated cards with proper ids

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function (){
+
+    var apiKey = "da7f79d7a0a51d7efc2cc54447adc5b9";
+    var searchInput = $("#search-input");
+    var searchBtn = $("search-btn");
+
+    function getCityInfo(e){
+        e.preventDefault();
+        
+    }
+
+
+});
