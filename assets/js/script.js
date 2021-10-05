3690ca39910f1b56f5af09261e90183b0060250f
isa <5445946+icortes@users.noreply.github.com>
2021-10-05 00:55:53 -0700

    cities are now saved in local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function (){
+$(document).ready(function () {
 
     var apiKey = "da7f79d7a0a51d7efc2cc54447adc5b9";
     var searchInput = $("#search-input");
@@ -6,74 +6,83 @@ $(document).ready(function (){
     var locationArr = [];
     var weatherData;
     var sideBar = $("#sidebar");
+    var searchedArray = [];
 
-    function getCityInfo(e){
+    function getCityInfo(e) {
         e.preventDefault();
 
         //get input from search bar
-        var userInput = searchInput.val().trim();
+        var userInput = searchInput.val().trim().toUpperCase();
         console.log(userInput);
-        
+
         //search the city for the user
         searchCityOf(userInput);
 
-        //create a button for the search
-        var newBtn = $("<button>");
-        newBtn.text(userInput.toUpperCase());
-        newBtn.addClass("btn btn-dark");
-        newBtn.appendTo(sideBar);
-        newBtn.on("click", getCityInfo);
+        //if statement that checks if the searched city has been searched before
+        //if it hasn't been searched make button for city
+        if (!(searchedArray.includes(userInput))) {
+            //create a button for the search
+            var newBtn = $("<button>");
+            newBtn.text(userInput);
+            newBtn.addClass("btn btn-dark");
+            newBtn.appendTo(sideBar);
+            newBtn.on("click", getCityInfo);
 
-        //add city to searched array
+            //add city to searched array
+            searchedArray.push(userInput);
+
+             //add to local storage
+             localStorage.setItem("searchedCities", JSON.stringify(searchedArray));
+        }
 
 
     }
 
-    function searchCityOf(input){
-         //get geolocation from api
-         var searchURL = "https://api.openweathermap.org/geo/1.0/direct?q=" + input + "&limit=3&appid=" + apiKey;
-         console.log(searchURL);
-         fetch(searchURL,{
-             cache: "reload",
-         })
-         .then(function(response){
-             console.log(response);
-             //if response is other than 'ok'
-             if(response.status !== 200){
-                 console.log("Invalid Input");
-             }
-             return response.json();
-         })
-         .then(function(data){
-             console.log(data);
-             locationArr = [data[0].lat,data[0].lon];
-             console.log([locationArr]);
-             //get weather data
-             getWeather();
-         })
+    function searchCityOf(input) {
+        //get geolocation from api
+        var searchURL = "https://api.openweathermap.org/geo/1.0/direct?q=" + input + "&limit=3&appid=" + apiKey;
+        console.log(searchURL);
+        fetch(searchURL, {
+                cache: "reload",
+            })
+            .then(function (response) {
+                console.log(response);
+                //if response is other than 'ok'
+                if (response.status !== 200) {
+                    console.log("Invalid Input");
+                }
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data);
+                locationArr = [data[0].lat, data[0].lon];
+                console.log([locationArr]);
+                //get weather data
+                getWeather();
+            })
     }
 
     //get weather data from lat and lon
-    function getWeather(){
+    function getWeather() {
         //create url for weather data
         var weatherURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + locationArr[0] + "&lon=" + locationArr[1] + "&units=imperial&appid=" + apiKey;
         console.log(weatherURL);
 
         //get weather data from api
         fetch(weatherURL, {
-            cache: "reload"
-        })
-        .then(function(response){
-            return response.json();
-        })
-        .then(function(data){
-            console.log(data);
-            weatherData = data;
-            makeElements();
-        })
+                cache: "reload"
+            })
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data);
+                weatherData = data;
+                makeElements();
+            })
     }
 
-    function makeElements(){
+    function makeElements() {
         //get data from weatherData
         var cityName = searchInput.val().toUpperCase();
         var todayDate = moment().format("MM/DD/YYYY");
@@ -81,12 +90,12 @@ $(document).ready(function (){
         var todayWindSpeed = weatherData.current.wind_speed;
         var todayHumidity = weatherData.current.humidity;
         var todayUV = weatherData.current.uvi;
-        var weatherIcon = "http://openweathermap.org/img/wn/"+ weatherData.current.weather[0].icon +"@2x.png";
+        var weatherIcon = "http://openweathermap.org/img/wn/" + weatherData.current.weather[0].icon + "@2x.png";
 
         //get card deck from the DOM
         var cardDeck = document.querySelector("#card-deck").children;
         console.log(cardDeck);
-        
+
         //add data to the DOM
         $("#city").text(cityName);
         $("#today-date").text(todayDate);
@@ -101,12 +110,12 @@ $(document).ready(function (){
         var dailyForecastArray = weatherData.daily;
         console.log(dailyForecastArray);
         //for loop to set daily forecasts to each card
-        for(var i = 0; i < 5; i++){
+        for (var i = 0; i < 5; i++) {
             //get date from array[i].dt and convert it from unix to MM/DD/YYYY
             var dailyDate = moment.unix(dailyForecastArray[i].dt).format("MM/DD/YYYY");
             console.log(dailyDate);
             //gets the weather icon
-            var dailyIcon = "http://openweathermap.org/img/wn/"+ dailyForecastArray[i].weather[0].icon + "@2x.png";
+            var dailyIcon = "http://openweathermap.org/img/wn/" + dailyForecastArray[i].weather[0].icon + "@2x.png";
             //gets the temp and fixes it to 2 decimal points
             var dailyTemp = dailyForecastArray[i].temp.day.toFixed(2);
             console.log(dailyTemp);
